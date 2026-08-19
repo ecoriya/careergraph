8be92e9d7009ce653463af1dc0593db539cb2e4d
ecoriya <riyakatikala@gmail.com>
2026-08-19 23:50:31 +0530

    Configure server for deployment

--- server/server.js
+++ server/server.js
@@ -190,6 +190,6 @@ app.get("/api/recommendations/:userId", async (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
