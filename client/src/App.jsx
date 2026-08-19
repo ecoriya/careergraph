ef15b32a795081ad69651aa5b9026abafa1b85f2
ecoriya <riyakatikala@gmail.com>
2026-08-20 00:18:53 +0530

    Connect frontend to deployed API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://careergraph-2y2e.onrender.com";
 
 function App() {
   const [user, setUser] = useState(null);
